4c5c5ecb9f7b7e0209a396e1cf593d9977e32196
charlie1099 <cdcharlesworth@gmail.com>
2021-11-07 22:10:37 -0700

    added employee table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,11 @@
 INSERT INTO employee
 (first_name, last_name, role_id, manager_id)
 VALUES
-("Jane", "Doe", 1, 2),
-("Mark", "John", )
+("Ricky", "Bobby", 1, NULL),
+("Cal", "Naughton", 2, 1),
+("Glenn", "McBrayer", 3, 2),
+("Lucius", "Washington", 4, NULL),
+("Kyle", "Roberts", 5, 2),
+("Susan", "Adams", 6, NULL),
+("Larry", "Dennit", 7, 3);
+
